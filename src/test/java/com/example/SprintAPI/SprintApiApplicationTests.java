c257061291be398eed2c0c549ee052cd9f230b0a
yashsrivastava0123 <137787136+yashsrivastava0123@users.noreply.github.com>
2025-07-24 17:59:24 +0530

    Update SprintApiApplicationTests.java

--- src/test/java/com/example/SprintAPI/SprintApiApplicationTests.java
+++ src/test/java/com/example/SprintAPI/SprintApiApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.SprintAPI;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+//Test
 @SpringBootTest
 class SprintApiApplicationTests {
 
